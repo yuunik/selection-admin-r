c250faea7465eaa12f5df3ff894008bc020b73af
yuunik <chorria.zhou@gmail.com>
2025-01-28 02:05:13 +0800

    feat: add axios config.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import 'virtual:svg-icons-register'
+
 import App from '@/App.tsx'
+
+import '@/styles/index.scss'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ src/utils/request.tsx
@@ -0,0 +1,56 @@
+import axios from 'axios'
+import { message } from 'antd'
+
+// 创建axios实例
+const request = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASE_URL,
+  timeout: 5000,
+})
+
+// 请求拦截器
+request.interceptors.request.use(
+  (config) => {
+    // 在发送请求之前做些什么
+    return config
+  },
+  (error) => {
+    // 对请求错误做些什么
+    return Promise.reject(error)
+  },
+)
+
+// 响应拦截器
+request.interceptors.response.use(
+  (response) => {
+    // 对响应数据做点什么
+    return response.data
+  },
+  (error) => {
+    // 错误信息
+    let msg = ''
+    // 错误状态码
+    const status = error.response.status
+    switch (status) {
+      case 401:
+        msg = 'token过期'
+        break
+      case 403:
+        msg = '无权访问'
+        break
+      case 404:
+        msg = '请求地址错误'
+        break
+      case 500:
+        msg = '服务器出现问题'
+        break
+      default:
+        msg = '无网络'
+    }
+    // 提示错误信息
+    message.error(msg)
+    // 对响应错误做点什么
+    return Promise.reject(error)
+  },
+)
+
+export default request
