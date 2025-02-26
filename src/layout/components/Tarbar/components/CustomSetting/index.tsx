6a13754db4ef694e8ed7eea43c2254557ed60230
yuunik <chorria.zhou@gmail.com>
2025-02-27 02:39:02 +0800

    feat: 新增全屏功能

--- src/layout/components/Tarbar/components/CustomSetting/index.tsx
+++ src/layout/components/Tarbar/components/CustomSetting/index.tsx
@@ -1,12 +1,14 @@
 import { Avatar, Button, Dropdown, Space, Tooltip } from 'antd'
 import {
   DownOutlined,
+  FullscreenExitOutlined,
   FullscreenOutlined,
   ReloadOutlined,
   SettingOutlined,
   UserOutlined,
 } from '@ant-design/icons'
 import { useDispatch } from 'react-redux'
+import { useEffect, useState } from 'react'
 
 import store from '@/store'
 import { handleKey } from '@/store/modules/setting'
@@ -16,10 +18,46 @@ import './index.scss'
 const CustomSetting = () => {
   // 获取dispatch
   const dispatch = useDispatch<typeof store.dispatch>()
+
   // 刷新页面
   const onRefresh = () => {
     dispatch(handleKey())
   }
+
+  // 全屏状态
+  const [isFullscreen, setIsFullscreen] = useState(false)
+  // 切换全屏
+  const onSizeChange = () => {
+    if (!document.fullscreenElement) {
+      // 进入全屏
+      document.documentElement.requestFullscreen()
+      setIsFullscreen(true)
+    } else {
+      // 退出全屏
+      document.exitFullscreen()
+      setIsFullscreen(false)
+    }
+  }
+
+  // 监听屏幕大小变化
+  const onResize = () => {
+    if (
+      window.screen.width === document.documentElement.clientWidth &&
+      document.documentElement.clientHeight === window.screen.height
+    ) {
+      setIsFullscreen(true)
+    } else {
+      setIsFullscreen(false)
+    }
+  }
+
+  // 监听屏幕大小变化
+  useEffect(() => {
+    window.addEventListener('resize', onResize)
+    return () => {
+      window.removeEventListener('resize', onResize)
+    }
+  }, [])
 
   return (
     <div className="website-setting">
@@ -32,7 +70,14 @@ const CustomSetting = () => {
         />
       </Tooltip>
       <Tooltip title="全屏">
-        <Button size="small" shape="circle" icon={<FullscreenOutlined />} />
+        <Button
+          size="small"
+          shape="circle"
+          icon={
+            isFullscreen ? <FullscreenExitOutlined /> : <FullscreenOutlined />
+          }
+          onClick={onSizeChange}
+        />
       </Tooltip>
       <Tooltip title="自定义设置">
         <Button size="small" shape="circle" icon={<SettingOutlined />} />
